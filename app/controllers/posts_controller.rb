e09d5fdcf4fdc3668b85bfc7d284c8cabe85eb97
Michael Cleyn <michael.cleyn@shopify.com>
2015-03-21 17:11:39 -0400

    added token and secret to client

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key    = ENV['TWITTER_KEY']
-  config.consumer_secret = ENV['TWITTER_SECRET']
-end
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key    = ENV['TWITTER_KEY']
+    config.consumer_secret = ENV['TWITTER_SECRET']
+    config.access_token = ENV ["YOUR_ACCESS_TOKEN"]
+    config.access_token_secret = ENV ["YOUR_ACCESS_SECRET"]
+  end
 
   def index
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320003515) do
+ActiveRecord::Schema.define(version: 20150320011225) do
+
+  create_table "posts", force: true do |t|
+    t.text     "status"
+    t.string   "handle"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
