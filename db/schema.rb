37869ac819a00b9be0ffa29f6c773dfdbe11b034
Don Currie <d.currie@utoronto.ca>
2015-03-26 21:26:14 -0400

    added associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320011225) do
+ActiveRecord::Schema.define(version: 20150327012152) do
 
   create_table "posts", force: true do |t|
     t.text     "status"
     t.string   "handle"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email"
