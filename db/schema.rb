12425a7105653697c190d1665f1f06f4dbc7bc05
Michael Cleyn <michael.cleyn@shopify.com>
2015-04-05 11:43:24 -0400

    add columns to users for devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405150614) do
+ActiveRecord::Schema.define(version: 20150405153146) do
 
   create_table "posts", force: true do |t|
     t.text     "status"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20150405150614) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
