0b7ddbfc64f3ff5f6bdfa3464904d9a4b1e511ee
Don Currie <d.currie@utoronto.ca>
2015-04-09 21:11:40 -0400

    fixed callbacks route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks"
+    omniauth_callbacks: "callbacks"
   }
 
   get '/tweets' => 'posts#tweets'
