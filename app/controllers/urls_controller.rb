0e069a69f77afbbd7ef4d106c1b186170ef26f21
Michael Cleyn <michael.cleyn@shopify.com>
2015-04-16 22:14:51 -0400

    simple changes to views

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,8 +1,14 @@
 class UrlsController < ApplicationController
-	def create
-		u = Url.new url: params[:url], user_id: current_user.id
-    	u.save
 
-    	render 'index'
+	def new
+		@url = Url.new
+		render 'index'
+	end
+
+	def create
+		@url = Url.new url: params[:url], user_id: current_user.id
+    @url.save
+
+    render 'index'
 	end
 end
