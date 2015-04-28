e8cb8ca41f104edeb9d7f0b802c30758d896d445
Michael Cleyn <michael.cleyn@shopify.com>
2015-04-27 21:16:32 -0400

    grab posts for specific day

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,10 @@
 class Url < ActiveRecord::Base
 	belongs_to :user
+
+	def self.scheduled_today
+		dow = Time.now.strftime("%A").downcase.to_s
+		query = {}
+		query[dow] = true
+		where query
+	end
 end
